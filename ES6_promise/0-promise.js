3746708bb00224eb9e1d71b8b7da9202bac52d00
DesaraB <desarabaroti99@gmail.com>
2025-04-02 14:35:30 +0200

    added new file

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+/*eslint-disable*/
+export default function getPromise() {
+	return new Promise((resolve, reject) => {
+	  setTimeout(() => {
+		resolve('Promise resolved!');
+	  }, 1000);
+	});
+  }
