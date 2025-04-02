533ec782e468f6e593b42ccdd12bc4ac032fa6c9
DesaraB <desarabaroti99@gmail.com>
2025-04-02 12:43:35 +0200

    added new file

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+/*eslint-disable*/
+export default function updateUniqItems(uniqItems) {
+    if (!(uniqItems instanceof Map)){
+        throw new Error('Cannot process');
+    }
+    uniqItems.forEach((value, key) => {
+        if (value === 1) {
+            uniqItems.set(key, 100);
+        }
+    });
+    return uniqItems
+}
