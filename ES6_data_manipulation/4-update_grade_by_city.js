2057a6113d4689cd5adbfb294d865ae25460f4d7
DesaraB <desarabaroti99@gmail.com>
2025-04-02 12:38:18 +0200

    added new file

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default function updateStudentGradeByCity(students, city, newGrades) { 
+	const studentsInCity = students.filter(student => student.location === city);
+  
+	return studentsInCity.map(student => {
+	  const newGrade = newGrades.find(grade => grade.studentId === student.id);
+	  return {
+		...student,
+		grade: newGrade ? newGrade.grade : 'N/A',
+	  };
+	});
+  }
