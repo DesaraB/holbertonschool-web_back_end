46d2771cb393d74a01ec7920b14118920c1dbcdb
DesaraB <desarabaroti99@gmail.com>
2025-04-04 12:44:29 +0200

    changes

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 export default function getResponseFromAPI(success) {
 	return new Promise((resolve, reject) => {
-		const success = true;
-		if (success) {
+		if (success === true) {
 			resolve({status: 200, body: 'Success'});
 		} else {
 			reject(new Error('The fake API is not working currently'));
